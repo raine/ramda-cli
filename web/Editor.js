abbd458e038f0a11ea2f99ca8280f52eca1318ed
Raine Virta <raine.virta@gmail.com>
2018-12-19 19:34:02 +0200

    show > at start of editor

--- web/Editor.js
+++ web/Editor.js
@@ -3,13 +3,21 @@ import { Controlled as CodeMirror } from 'react-codemirror2'
 
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
+// import 'codemirror/theme/midnight.css'
+// import 'codemirror/theme/monokai.css'
+// import 'codemirror/theme/pastel-on-dark.css'
+// import 'codemirror/theme/seti.css'
+// import 'codemirror/theme/shadowfox.css'
+// import 'codemirror/theme/tomorrow-night-eighties.css'
 import 'codemirror/mode/shell/shell'
 import 'codemirror/addon/display/placeholder'
 import style from './styles/Editor.scss'
 
 const Editor = ({ value, onChange, placeholder }) => (
-  <div>
+  <div className={style.editor}>
+    <div className={style.prefix}>&gt;</div>
     <CodeMirror
+      className={style.codemirror}
       value={value}
       options={{
         mode: 'shell',
