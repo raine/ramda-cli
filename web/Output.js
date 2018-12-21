3f322ab884b94546e63d76717dae3352847d47ff
Raine Virta <raine.virta@gmail.com>
2018-12-21 21:41:05 +0200

    use react-window to improve performance with massive output

--- web/Output.js
+++ web/Output.js
@@ -1,30 +1,68 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import stripAnsi from 'strip-ansi'
 import { pure } from 'recompose'
 import classNames from 'classnames'
 import ansi2html from './ansi2html'
 import isSafari from './is-safari'
+import { FixedSizeList } from 'react-window'
+import AutoSizer from 'react-virtualized-auto-sizer'
 
 import style from './styles/Output.scss'
 
-const errorOutput = (err) =>
-  isSafari ? err.toString() : err.stack
+const errorOutput = (err) => (isSafari ? err.toString() : err.stack)
 
-const Output = ({ output, outputType, error }) => (
-  <div className={classNames(style.output, { [style.error]: error })}>
-    {error && <pre>{errorOutput(error)}</pre>}
-    {!error && ['pretty', 'table'].includes(outputType) ? (
-      <pre
-        dangerouslySetInnerHTML={ansi2html(output, {
-          escapeXML: true,
-          fg: '#afafaf',
-          bg: '#0a0a0a'
-        })}
-      />
+const hasAnsiColors = (outputType) => ['pretty', 'table'].includes(outputType)
+
+const AnsiPre = pure(({ str, style }) => (
+  <pre
+    style={style}
+    dangerouslySetInnerHTML={ansi2html(str, {
+      escapeXML: true,
+      fg: '#afafaf',
+      bg: '#0a0a0a'
+    })}
+  />
+))
+
+class OutputRow extends PureComponent {
+  render() {
+    const { data, index, style: rwStyle, outputType } = this.props
+    const content = data[index]
+    return hasAnsiColors(outputType) ? (
+      <AnsiPre style={rwStyle} str={content} />
     ) : (
-      <pre>{output}</pre>
-    )}
-  </div>
-)
+      <pre style={rwStyle}>{content}</pre>
+    )
+  }
+}
 
-export default pure(Output)
+class Output extends PureComponent {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { outputType, output, error } = this.props
+    const lines = (error ? errorOutput(error) : output).split('\n')
+
+    return (
+      <div className={classNames(style.output, { [style.error]: error })}>
+        <AutoSizer disableWidth={true}>
+          {({ height }) => (
+            <FixedSizeList
+              className={style.outputList}
+              height={height}
+              itemData={lines}
+              itemCount={lines.length}
+              itemSize={24}
+            >
+              {(props) => <OutputRow {...props} outputType={outputType} />}
+            </FixedSizeList>
+          )}
+        </AutoSizer>
+      </div>
+    )
+  }
+}
+
+export default Output
