4f4f753697e05da9909105f065904ad9e0ff585a
Raine Virta <raine.virta@gmail.com>
2018-12-24 18:57:32 +0200

    stream stdin in browser

--- web/index.js
+++ web/index.js
@@ -2,14 +2,19 @@ import React from 'react'
 import { render } from 'react-dom'
 import querystring from 'querystring'
 import App from './App'
+import http from 'stream-http'
 
 import './styles/reboot.css'
 import './styles/main.css'
 
+let stdin = ''
 const getStdin = () =>
-  window
-    .fetch('/stdin')
-    .then((res) => res.text())
+  http.get('stdin', (res) => {
+    res.on('data', (buf) => {
+      stdin += buf.toString()
+      doRender({ stdin })
+    })
+  })
 
 const doRender = (props) =>
   render(<App {...props} />, document.getElementById('root'))
@@ -17,18 +22,13 @@ const doRender = (props) =>
 const { input } = querystring.parse(window.location.href.split('?')[1])
 
 doRender({ stdin: null, input })
-
-getStdin().then((str) => {
-  doRender({ stdin: str })
-})
+getStdin()
 
 const aliveCheck = () => {
-  window.ALIVE_CHECK = window
-    .fetch('/alive-check')
-    .catch((err) => {
-      console.error(err)
-      setTimeout(aliveCheck, 100)
-    })
+  window.ALIVE_CHECK = window.fetch('/alive-check').catch((err) => {
+    console.error(err)
+    setTimeout(aliveCheck, 100)
+  })
 }
 
 if (!window.ALIVE_CHECK) aliveCheck()
